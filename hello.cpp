cd5572b2cec65c626cea24876348e311e151051d
polinaerlih <ketsi201198@mail.ru>
2019-10-22 15:50:36 +0500

    +5

--- hello.cpp
+++ hello.cpp
@@ -1,18 +1,8 @@
-#include<iostream>
-using namespace std;
-main(){
-char s[1000];
-cin>>s;	
-bool IsBreak=false;
-for (int i=0; s[i]!='\0'; i++) 
-if (s[i]<48 || s[i]>57) {
-cout<<"S not number"; 
-IsBreak=true; 
-break;
-}
-<<<<<<< HEAD
-else cout<<"Hello world "<<s<<" "<<7;
-=======
-else cout<<"Hello world "<<s<<" "<<5;
->>>>>>> feature/adding_5_number
+#include <iostream>
+
+int main(){
+   cout << "Hello world";
+   int delta;
+   cin >> delta;
+   cout << delta +5;
 }
